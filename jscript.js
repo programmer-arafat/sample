8ca81b12f3c665185f71ec2db560d213b795a35e
Arafat <arafat.freelance1@gmail.com>
2023-10-13 01:38:35 +0530

    more changes

--- jscript.js
+++ jscript.js
@@ -1,16 +1,26 @@
 const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener('click', ()=>{
-    document.querySelector(".booking").style.display = "none";
-    document.querySelector(".wrapper").style.display = "none";
-    document.getElementById("hamburger").style.display = "none";
+    // document.querySelector(".booking").style.display = "none";
+    // document.querySelector(".wrapper").style.display = "none";
+    // document.getElementById("hamburger").style.display = "none";
+    // const openmenu = document.querySelector(".hamburger-menu");
+    // openmenu.classList.add("hamburger-menu-visible");
+    document.querySelector('.booking').style.display = "none";
+    document.querySelector(".wrapper").style.width = "0px";
     const openmenu = document.querySelector(".hamburger-menu");
     openmenu.classList.add("hamburger-menu-visible");
 })
 const closeBtn = document.querySelector(".close-btn");
 closeBtn.addEventListener('click', ()=>{
-    document.querySelector(".booking").style.display = "flex";
-    document.querySelector(".wrapper").style.display = "block";
-    document.getElementById("hamburger").style.display = "block";
+    document.querySelector('.booking').style.display = "none";
+    document.querySelector(".wrapper").style.width = "100vw";
     const openmenu = document.querySelector(".hamburger-menu");
     openmenu.classList.remove("hamburger-menu-visible");
+})
+const dropList = document.getElementById("dropdown");
+dropList.addEventListener('click', ()=>{
+    const dropdownMenu = document.querySelector('#dropmenu');
+    const arrow = document.getElementById('dropArrow');
+    arrow.classList.toggle("arrow-up");
+    dropdownMenu.classList.toggle("dropdown-links");
 })
