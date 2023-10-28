b92f6a846b9876d42ef57d616da05c47229f7b2c
Arafat <arafat.freelance1@gmail.com>
2023-10-29 00:14:00 +0530

    more features added

--- jscript.js
+++ jscript.js
@@ -1,6 +1,7 @@
 const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener('click', ()=>{
     document.querySelector("#footer").style.display = "none";
+    document.querySelector(".login-box").style.display = "none";
     document.querySelector('.booking').style.display = "none";
     document.querySelector(".wrapper").style.display = "none";
     const openmenu = document.querySelector(".hamburger-menu");
@@ -9,6 +10,7 @@ hamburger.addEventListener('click', ()=>{
 const closeBtn = document.querySelector(".close-btn");
 closeBtn.addEventListener('click', ()=>{
     document.querySelector("#footer").style.display = "block";
+    document.querySelector(".login-box").style.display = "block";
     document.querySelector('.booking').style.display = "flex";
     document.querySelector(".wrapper").style.display = "block";
     const openmenu = document.querySelector(".hamburger-menu");
